c8b1af7df757573265a754d1a31fbaa2929f00bd
ToniHaddad <tonishaddad@gmail.com>
2023-10-29 23:44:53 +0200

    100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,44 @@
 #include "main.h"
 
 /**
- * _atoi - convert a string to an integer.
- * @s: string to be converted
+ * _atoi - Convert a string to an integer.
+ * @s: String to be converted.
  *
- * Return: the integer value of the converted string
+ * Return: Integer value of the converted string.
  */
 int _atoi(char *s)
 {
-    int res = 0;     /* to store the result */
-    int sign = 1;    /* to store the sign (1 for positive, -1 for negative) */
-    int started = 0; /* to indicate if the number parsing has started */
+    int res = 0;     /* To store the result. */
+    int sign = 1;    /* To store the sign (1 for positive, -1 for negative). */
+    int started = 0; /* To indicate if the number parsing has started. */
 
     while (*s)
     {
-        /* check for minus sign */
+        /* Check for minus sign. */
         if (*s == '-')
             sign *= -1;
-        /* if it's a number */
+
+        /* If it's a number. */
         if (*s >= '0' && *s <= '9')
         {
             started = 1;
+
+            /* Check for overflow. */
+            if (res > (INT_MAX - (*s - '0')) / 10)
+            {
+                /* Handle overflow based on the sign. */
+                if (sign == 1)
+                    return INT_MAX;
+                else
+                    return INT_MIN;
+            }
+
             res = res * 10 + (*s - '0');
         }
-        /* if it's not a number and the number parsing has started, break */
+        /* If it's not a number and the number parsing has started, break. */
         else if (started)
             break;
+
         s++;
     }
 
