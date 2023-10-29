40532581d4a1998c454732c768d7a2553a94f38e
ToniHaddad <tonishaddad@gmail.com>
2023-10-29 22:44:46 +0200

    main

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -1,18 +1,41 @@
 #include "main.h"
 #include <stdio.h>
 
+#include <stdio.h>
+
+int _strlen(char *);
+
 /**
- * main - check the code
+ * _strlen - Computes the length of a string.
+ * @s: The string to measure the length of.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+    int length = 0;
+
+    while (*s)
+    {
+        length++;
+        s++;
+    }
+
+    return length;
+}
+
+/**
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int n;
+    char *str;
+    int len;
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
+    str = "Holberton!";
+    len = _strlen(str);
+    printf("%d\n", len);
     return (0);
 }
